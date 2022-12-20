5c699a9d47dc18416e4faa5c8a8c096f03ac8e1e
Juan Carlos <80911763+lordbael@users.noreply.github.com>
2022-12-20 09:15:08 -0500

    instalación de mqtt

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import * as mqtt from 'mqtt';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-
+  
   ngOnInit(): void {
-  }
 
+    //instancia
+    const client = mqtt.connect('mqtt://test.mosquitto.org');
+
+    //sub
+    client.subscribe('my_topic');
+
+    client.on('message', (topic, message) => {
+      console.log(`Received message ${message} on topic ${topic}`);
+    });
+
+    //mensaje
+    client.publish('my_topic', 'Hello MQTT');
+
+  }
+  
 }
